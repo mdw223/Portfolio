fefefbfb2a5024700d30b83ec692cc7827e18c99
Malik <malikadidw@gmail.com>
2025-08-11 22:28:57 -0400

    fixes to site, plus config for building assets

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/' 
+  base: '/Portfolio/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx', // Your main entry file
+        // Add other entry points if needed
+      },
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]', // Customize asset naming
+      },
+    },
+  },
 })
